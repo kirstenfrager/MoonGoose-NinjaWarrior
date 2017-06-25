dcb16eec8ce0966dbf786dcf7dcfd41dce38b1ae
Kirsten Frager <kirstenfrager@192-168-1-5.tpgi.com.au>
2017-06-25 12:58:07 +1000

    static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def bio
+  end
+
+  def packages
+  end
+
+  def merch
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def cart
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/bio'
+
+  get 'pages/packages'
+
+  get 'pages/merch'
+
+  get 'pages/contact'
+
+  get 'pages/faq'
+
+  get 'pages/cart'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
