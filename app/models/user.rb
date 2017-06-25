c12bd726f1ba333a1e2e2c407694c8406e6c0fa8
Kirsten Frager <kirstenfrager@192-168-1-5.tpgi.com.au>
2017-06-25 15:58:59 +1000

    rolify implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :packages
+
+  after_create :assign_default_role
+
+# default role as member created when someone signs up
+  def assign_default_role
+    self.add_role(:member) if self.roles.blank?
+  end
+
+  def admin?
+    has_role?(:admin)
+  end
 end
